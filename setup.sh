e130289845be7847d070d3f277b1c4d11c675911
chris-1187 <christian.munz@posteo.de>
2025-07-11 13:03:45 +0200

    ghcr setup
    Signed-off-by: chris-1187 <christian.munz@posteo.de>

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Build image locally; Use when something changed
+# For local testing: Build image locally; Use when something changed
 # docker build -t scalability-demo:latest .
 
 # Start the minikube cluster with enough resources for up to 9 Nodes
@@ -21,13 +21,24 @@ cd autoscaler/vertical-pod-autoscaler || exit 1
 cd ../..
 
 # Load the image into minikube
-minikube image load scalability-demo:latest
+minikube image load ghcr.io/chris-1187/scalability-demo:latest
 
 # apply deployment
 kubectl apply -f scalability-demo-deployment.yaml
+sleep 5
 
-for i in {1..10}; do
+while true; do
+    clear
+    echo -e "Waiting for 9 pods to be in 'Running' status...\n"
     kubectl get pods
+
+    running_count=$(kubectl get pods --no-headers | grep 'Running' | wc -l)
+
+    if [ "$running_count" -ge 9 ]; then
+        echo -e "\n> 9 pods are running"
+        break
+    fi
+
     sleep 1
 done
 
